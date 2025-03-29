1362a5a9f6de76a8acb16e92bc0eb30195bb5671
Shrijith <shrijith2910@gmail.com>
2025-03-29 17:04:22 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+app.mount("/assets",StaticFiles(directory="assets"),name="assets")
+
+def profile_html():
+    with open("templates/index.html","r") as html_file:
+        content = html_file.read()
+    return HTMLResponse(content=content, status_code = 200)
+    
+@app.get("/")
+def home():
+    return profile_html()
