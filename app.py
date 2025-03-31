7a6daba03c88e885a740783b91e140b5811f9694
shrijith-shetty <151986938+shrijith-shetty@users.noreply.github.com>
2025-03-31 17:44:31 +0000

    changes in index folder from template to profile

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-app.mount("/assets",StaticFiles(directory="assets"),name="assets")
 
-def profile_html():
-    with open("templates/index.html","r") as html_file:
-        content = html_file.read()
-    return HTMLResponse(content=content, status_code = 200)
-    
-@app.get("/")
+# Mount static assets
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+
+# Serve HTML file directly
+@app.get("/", response_class=FileResponse)
 def home():
-    return profile_html()
+    return "index.html"
+
+# python -m venv .venv
+#python -m http.server 3333(port numebr)
